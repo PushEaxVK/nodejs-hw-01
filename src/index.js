88a8309e774c92ac4ac896734793313a4e118954
PushEax <pusheax.vk@gmail.com>
2025-05-24 10:43:59 +0300

    db read/write

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
 import { PATH_DB } from './constants/contacts.js';
+import { readContacts } from './utils/readContacts.js';
+import { writeContacts } from './utils/writeContacts.js';
 console.log(PATH_DB);
+
+(async () => {
+  const contacts = await readContacts();
+  console.log(contacts);
+  contacts.push('Hello');
+  console.log(contacts);
+  await writeContacts(contacts);
+})();
